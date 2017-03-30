1949e9ba5846f17db454b584c42527dea4d318da
Kieron Woodhouse <kieron.woodhouse@yahoo.co.uk>
2017-03-30 15:17:10 +0000

    Added a sanity check for the returned price to ensure it has the format of a price (i.e. '£X.XX') Tidied up code.

--- call-cost-scraper.py
+++ call-cost-scraper.py
@@ -1,46 +1,40 @@
 # coding: utf-8
+import re
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Assuming you want the standard landline rate on a pay monthly contract...
-
-# Steps to find call cost
-# 1. Navigate to the link:
-#   http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk
-# 2. Using input#countryName...
-# 3. Enter the country name and press Enter
-# 4. Click a#paymonthly
-# 5. Look in table#standardRatesTable for
-#    <td>Landline</td><td>*XXXX*</td>
-
 class CallCostScraper:
     def __init__(self, target_url):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(10) # seconds
         self.target_url = target_url
-        # self.load_page()
 
     def load_page(self):
         self.driver.get(self.target_url)
 
     def lookup_country(self, country):
         input_elem = self.driver.find_element_by_id("countryName")
-        # TODO: Check if element is the input we're after
+
         input_elem.clear()
         input_elem.send_keys(country)
         input_elem.send_keys(Keys.RETURN)
 
         wait = WebDriverWait(self.driver, 10)
         paym_elem = wait.until(EC.element_to_be_clickable((By.ID, 'paymonthly')))
-        # paym_elem = self.driver.find_element_by_xpath("//*[@id='paymonthly']")
         paym_elem.click()
 
     def find_rate(self):
         price_elem = self.driver.find_element_by_xpath("//*[@id='standardRatesTable']/tbody/tr[1]/td[contains(text(),'Landline')]/following-sibling::td[1]")
-        # TODO: Check we got a price
+        # There is an assumption that prices are between £1.00 and £9.99, or
+        # otherwise take the form £0.XX if less than £1.00.
+        prog = re.compile(ur"^£[0-9]\.[0-9]{2}$", re.UNICODE)
+        if prog.match(price_elem.text) == None:
+            print("Error! Value returned was not a price.")
+            return None
         return unicode(price_elem.text)
 
     def get_country_price(self, country):
@@ -50,9 +44,11 @@ class CallCostScraper:
 
     pass
 
-scraper = CallCostScraper("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
+TGT_URL = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
 countries_to_lookup = ['Canada', 'Germany', 'Iceland', 'Pakistan', 'Singapore', 'South Africa']
+
+scraper = CallCostScraper(TGT_URL)
 
 for country in countries_to_lookup:
     price = scraper.get_country_price(country)
-    print("Price for " + country + ": " + price)
+    print("Price for " + country + ": " + unicode(price))
