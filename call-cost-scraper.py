d291c9e754cd74979396091a562c040b81dc90cf
Kieron Woodhouse <kieron.woodhouse@yahoo.co.uk>
2017-03-30 14:25:45 +0000

    Added the full set of countries to get prices for. Fixed any obvious bugs. Added a wait to ensure the '#paymonthly' button is clickable, which seemed to fix some apparently non-deterministic bugs.

--- call-cost-scraper.py
+++ call-cost-scraper.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Assuming you want the standard landline rate on a pay monthly contract...
 
@@ -16,8 +19,9 @@ from selenium.webdriver.common.keys import Keys
 class CallCostScraper:
     def __init__(self, target_url):
         self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10) # seconds
         self.target_url = target_url
-        self.load_page()
+        # self.load_page()
 
     def load_page(self):
         self.driver.get(self.target_url)
@@ -29,22 +33,26 @@ class CallCostScraper:
         input_elem.send_keys(country)
         input_elem.send_keys(Keys.RETURN)
 
-        self.driver.implicitly_wait(5) # seconds
-        paym_elem = self.driver.find_element_by_id("paymonthly")
+        wait = WebDriverWait(self.driver, 10)
+        paym_elem = wait.until(EC.element_to_be_clickable((By.ID, 'paymonthly')))
+        # paym_elem = self.driver.find_element_by_xpath("//*[@id='paymonthly']")
         paym_elem.click()
 
     def find_rate(self):
         price_elem = self.driver.find_element_by_xpath("//*[@id='standardRatesTable']/tbody/tr[1]/td[contains(text(),'Landline')]/following-sibling::td[1]")
-        # TODO: Check it's the right row of the table
         # TODO: Check we got a price
-        return price_elem.text
+        return unicode(price_elem.text)
 
     def get_country_price(self, country):
+        self.load_page()
         self.lookup_country(country)
         return self.find_rate()
 
     pass
 
 scraper = CallCostScraper("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
-canada_price = scraper.get_country_price("Canada")
-print("Price for Canada: " + unicode(canada_price))
+countries_to_lookup = ['Canada', 'Germany', 'Iceland', 'Pakistan', 'Singapore', 'South Africa']
+
+for country in countries_to_lookup:
+    price = scraper.get_country_price(country)
+    print("Price for " + country + ": " + price)
